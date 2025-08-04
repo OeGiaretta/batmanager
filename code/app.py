20b35bf6f67b1ebfc8639ce9989554ed8d5f4ed0
OeGiaretta <eduhgiaretta@gmail.com>
2025-08-04 17:04:06 -0300

    BottomBar e botões.
    Foi criado um BottomBar onde ficará os botões e a lista dos scripts.

--- code/app.py
+++ code/app.py
@@ -7,6 +7,36 @@ app.geometry('900x650')
 app._set_appearance_mode('dark')
 
 
+# BottomBar e botões
+
+bottombar = ctk.CTkFrame(app, height=250, corner_radius=0)
+bottombar.pack(side="bottom", fill="x")
+bottombar._set_appearance_mode('dark')
+
+buttonIncluir = ctk.CTkButton(bottombar, text="Incluir", command=0 )
+buttonIncluir.place(x=715, y=45 )
+buttonIncluir._set_appearance_mode('dark')
+
+
+buttonAlterar = ctk.CTkButton(bottombar, text="Incluir", command=0 )
+buttonAlterar.place(x=715, y=90 )
+buttonAlterar._set_appearance_mode('dark')
+
+
+buttonExcluir = ctk.CTkButton(bottombar, text="Incluir", command=0 )
+buttonExcluir.place(x=715, y=135 )
+buttonExcluir._set_appearance_mode('dark')
+
+
+buttonExecutar = ctk.CTkButton(bottombar, text="Incluir", command=0 )
+buttonExecutar.place(x=715, y=180 )
+buttonExecutar._set_appearance_mode('dark')
+
+# List
+
+
+
+
 
 
 
